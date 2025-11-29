878ad1cd709bbd86d19f7efc40de7237c6d6055b
semyon0101 <k.semyon.p@gmail.com>
2025-11-29 23:36:59 +0300

    I don't know why it doesn't work

--- app/query/route.ts
+++ app/query/route.ts
@@ -4,20 +4,20 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
 	const data = await sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
-  `;
+		SELECT invoices.amount, customers.name
+		FROM invoices
+		JOIN customers ON invoices.customer_id = customers.id
+		WHERE invoices.amount = 666;
+	`;
 
 	return data;
 }
 
 export async function GET() {
-  
-  try {
-  	return Response.json(await listInvoices());
-  } catch (error) {
-    	return Response.json({ error }, { status: 500 });
-  }
+
+	try {
+		return Response.json(await listInvoices());
+	} catch (error) {
+		return Response.json({ error }, { status: 500 });
+	}
 }
